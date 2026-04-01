16a23ac20827cf4a2428545db72d225e3c5475d2
Joseph Boyd <boydjr7@gmail.com>
2026-04-01 12:38:56 -0400

    setup_wgs_files

--- dev_wgs_workflow.R
+++ dev_wgs_workflow.R
@@ -4,11 +4,7 @@ library(ggplot2)
 # Example WGS workflow using EBVhelpR helper functions.
 # Update `wgs_root_dir` to your local WGS analysis directory if needed.
 
-wgs_root_dir <- "C:/Users/boydj/OneDrive - UVM Larner College of Medicine/projects_ashley/EBV_DLBCL/P2_viral_WGS"
-stopifnot(dir.exists(wgs_root_dir))
-# wgs_bwa_dir <- file.path(wgs_root_dir, "output_bwa")
-count_dir <- file.path(wgs_root_dir, "chr_read_counts")
-bigwig_dir <- file.path(wgs_root_dir, "bigwigs")
+
 
 # stopifnot(dir.exists(wgs_bwa_dir))
 stopifnot(dir.exists(count_dir))
@@ -17,12 +13,43 @@ stopifnot(dir.exists(bigwig_dir))
 meta_df <- load_meta_data()
 
 # 1) Build reference GRanges from BAM header.
-if(FALSE){
-    sizes_files = file.path(wgs_root_dir, "chrSizes.txt")
-    file.copy(sizes_files, "inst/extdata/WGS_chrSizes.txt")
+genome_gr <- load_wgs_reference_genome()
+
+setup_wgs_files = function(){
+    win_dir = "C:/Users/boydj/OneDrive - UVM Larner College of Medicine/projects_ashley/EBV_DLBCL/P2_viral_WGS"
+    lin_dir = "/gpfs1/pi/avolaric/files_jrboyd/P2_viral_WGS"
+    wgs_root_dir = win_dir
+    if(!dir.exists(wgs_root_dir)){
+        wgs_root_dir = lin_dir
+        if(!dir.exists(wgs_root_dir)){
+            stop("Could not locate WGS data directory.")
+        }
+    }
+
+    # wgs_bwa_dir <- file.path(wgs_root_dir, "output_bwa")
+    count_dir <- file.path(wgs_root_dir, "chr_read_counts")
+    bigwig_dir <- file.path(wgs_root_dir, "bigwigs")
+    stopifnot(dir.exists(count_dir))
+    stopifnot(dir.exists(bigwig_dir))
+
+    count_files = dir(count_dir, full.names = TRUE)
+    count_df = data.frame(count_file = count_files)
+    count_df = count_df %>% dplyr::mutate(sample_id = sub("_read.+", "", basename(count_file)))
+
+    bw_files = dir(bigwig_dir, pattern = "norm", full.names = TRUE)
+    bw_df = data.frame(bigwig = bw_files)
+    bw_df = bw_df %>% dplyr::mutate(sample_id = sub("_dedu.+", "", basename(bigwig)))
+    wgs_df = merge(count_df, bw_df, by = "sample_id", all = TRUE)
+    #some samples end in extra 2, redone?
+    wgs_df = wgs_df %>% dplyr::mutate(sample_id = ifelse(grepl("[0-9]_1$", sample_id), sub("_1$", "", sample_id), sample_id))
+    wgs_df = wgs_df %>% dplyr::mutate(sample_id = ifelse(grepl("[0-9]_2$", sample_id), sub("_2$", "", sample_id), sample_id))
+    wgs_df = merge(wgs_df, meta_df, by = "sample_id", all.x = TRUE)
+    stopifnot(!any(is.na(wgs_df$EBER_status)))
+    wgs_df %>% dplyr::filter(is.na(EBER_status))
+    wgs_df
 }
 
-genome_gr <- load_wgs_reference_genome()
+wgs_df = setup_wgs_files()
 
 # 2) Compute host/viral read summary metrics.
 wgs_count_summary <- load_wgs_count_summary(
